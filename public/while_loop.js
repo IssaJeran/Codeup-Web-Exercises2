b78056cfd0e5a71afc1c7829f258adacb7de9b93
IssaJeran <jeransmith22@gmail.com>
2017-02-15 08:00:43 -0600

    New Files From Exercises Yesterday with Javascript

---
+++ public/while_loop.js
@@ -0,0 +1,16 @@
+'use strict';
+
+ // This is how you get a random number between 50 and 100
+ var coneInventory = Math.floor(Math.random() * 50) + 50;
+ // This is how you get a random number between 1 and 5
+ var conesBoughtPerPerson = Math.floor(Math.random() * 5) + 1;
+ var conesSold = coneInventory
+ var allCones = (100 - coneInventory);
+
+do {
+	
+	
+} while (allCones > 0);
+    
+
+
